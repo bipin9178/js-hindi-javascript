16463cb955a0eefc0c518973511051d680f723cd
bipin9178 <bipinpkanzariya@gmail.com>
2025-07-15 06:24:28 +0000

    variables define

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId= 144553
+let accountEmail = "bipin@gmail.com"
+var accountPasswod= "12345"
+accountCity ="botad"
+let accountState
+
+// accountId =2 not allowed
+
+accountEmail = "bipin637362@gmail.com";
+accountPasswod = "673546435"
+accountCity = "Bengaluru"
+
+
+/*
+Prefer not to use 
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPasswod,accountCity,accountState])
